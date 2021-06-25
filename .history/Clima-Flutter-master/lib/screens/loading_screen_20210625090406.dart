681e2f94144ab8c26b1bf4c292ea57a4cb500bd8
Pial, Hamidur Rahaman <pial0007l@gmail.com>
2021-06-25 10:51:36 +0930

    get location

---
+++ .history/Clima-Flutter-master/lib/screens/loading_screen_20210625090406.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:location/location.dart';
+
+class LoadingScreen extends StatefulWidget {
+  @override
+  _LoadingScreenState createState() => _LoadingScreenState();
+}
+
+class _LoadingScreenState extends State<LoadingScreen> {
+  Location location = new Location();
+  Location location = new Location();
+
+bool _serviceEnabled;
+PermissionStatus _permissionGranted;
+LocationData _locationData;
+
+_serviceEnabled = await location.serviceEnabled();
+if (!_serviceEnabled) {
+  _serviceEnabled = await location.requestService();
+  if (!_serviceEnabled) {
+    return;
+  }
+}
+
+_permissionGranted = await location.hasPermission();
+if (_permissionGranted == PermissionStatus.denied) {
+  _permissionGranted = await location.requestPermission();
+  if (_permissionGranted != PermissionStatus.granted) {
+    return;
+  }
+}
+
+_locationData = await location.getLocation();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            //Get the current location
+          },
+          child: Text('Get Location'),
+        ),
+      ),
+    );
+  }
+}
---
+++ .history/Clima-Flutter-master/lib/screens/loading_screen_20210625090801.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:location/location.dart';
+
+class LoadingScreen extends StatefulWidget {
+  @override
+  _LoadingScreenState createState() => _LoadingScreenState();
+}
+
+class _LoadingScreenState extends State<LoadingScreen> {
+
+  void GetLocation (){
+    Position position = awat
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            //Get the current location
+          },
+          child: Text('Get Location'),
+        ),
+      ),
+    );
+  }
+}
